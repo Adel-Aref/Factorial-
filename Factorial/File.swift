b7ab771a3005f75ecb6e62271d0b7f28b9107aae
Adel <adel.aref16@gmail.com>
2018-04-30 14:52:19 -0700

    this is first commit

--- Factorial/File.swift
+++ Factorial/File.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+class Factorial{
+    func getRecurrionFactorial(num : Int) -> Int
+    {
+        if num == 1 || num == 0
+        {
+            return 1
+        }
+        else
+        {
+            return num*getRecurrionFactorial(num: num-1)
+        }
+    }
+    // for loooooooop
+    func getRepeatFactorial( num : Int) -> Int {
+        var num = num
+        var fact = 1
+        if num == 0 || num == 1
+        {fact = 1}
+        else
+        {
+            for _ in 1 ... num
+            {
+                fact = fact*num
+                num = num-1
+            }
+        }
+        return fact
+    }
+// power function 
+    func pow (num :Int , pow :Int ) ->  Int{
+        var res = 1
+        var pow = pow-1
+        
+        repeat
+        {
+        res *= num
+        pow -= 1
+        }
+        while pow >= 0
+        return res
+    }
+}
--- Factorial/ViewController.swift
+++ Factorial/ViewController.swift
@@ -9,7 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // here I define the global data members to be seen in my whole app
+    var fact = Factorial()
+    
+    @IBOutlet weak var textFactorial: UITextField!
 
+    @IBOutlet weak var TextPower: UITextField!
+    @IBOutlet weak var textToPower: UITextField!
+    @IBOutlet weak var labelShowFactorial: UILabel!
+    /////////
+    @IBAction func GetFactorialByRecurrsion(_ sender: Any) {
+    // here impelmenation of the recurrion button
+        let numFactorial  = textFactorial.text
+        let numberCharacters = NSCharacterSet.decimalDigits.inverted
+        if !(numFactorial?.isEmpty)! && numFactorial?.rangeOfCharacter(from: numberCharacters) == nil
+        {
+            let  transofermedNumFactorial :Int = (Int)(numFactorial!)!
+            labelShowFactorial.text =  (String)(fact.getRecurrionFactorial(num: transofermedNumFactorial))
+        }
+        else
+        {
+            labelShowFactorial.text = "Please Enter a valid salary "
+        }
+        
+        
+    }
+    @IBAction func getFactorialByRepeatLoop(_ sender: Any) {
+        // here impelmenation of the repeat button
+        let numFactorial  = textFactorial.text
+        let numberCharacters = NSCharacterSet.decimalDigits.inverted
+        if !(numFactorial?.isEmpty)! && numFactorial?.rangeOfCharacter(from: numberCharacters) == nil
+        {
+            let  transofermedNumFactorial :Int = (Int)(numFactorial!)!
+            labelShowFactorial.text =  (String)(fact.getRepeatFactorial(num: transofermedNumFactorial))
+        }
+        else
+        {
+            labelShowFactorial.text = "Please Enter a valid salary "
+        }
+    }
+    
+    @IBAction func getPower(_ sender: Any) {
+        // here impelmenation of the power button
+        let number  = textToPower.text
+        let power = TextPower.text
+        let numberCharacters = NSCharacterSet.decimalDigits.inverted
+        if !(number?.isEmpty)! && !(power?.isEmpty)! &&
+            number?.rangeOfCharacter(from: numberCharacters) == nil
+            && power?.rangeOfCharacter(from: numberCharacters) == nil
+        {
+            let  transofermedNum:Int = (Int)(number!)!
+            let  transofermedPower:Int = (Int)(power!)!
+
+            labelShowFactorial.text =  (String)(fact.pow(num: transofermedNum, pow: transofermedPower))
+        }
+        else
+        {
+            labelShowFactorial.text = "Please Enter a valid salary "
+        }
+        
+    }
+    // powerrrrrrrrrr
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
